c572df370908e68736989f4c22ba71a1342b5aa9
marlon olmedo <marlonolmedo240@gmail.com>
2021-04-11 21:08:34 -0600

    aprendiendo rutas

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home.index');
+
+Route::get('/contact', function () {
+    return 'conatct';
+})->name('home.contact');
+
+Route::get('/post/{id}', function ($id) {
+    return 'blog post '.$id;
+})
+// ->where([
+//     'id' => '[0-9]+'
+// ])
+->name("post.show");
+
+Route::get('/resent-post/{id?}', function ($daysago = 20) {
+    return 'post from '.$daysago .' days ago';
+})->name("post.recent.index");
