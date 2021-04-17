dfeb98c44542303b718ed9af907406e6b2f335c0
marlon olmedo <marlonolmedo240@gmail.com>
2021-04-16 20:22:10 -0600

    edicion de archibos
    se agregaron rutas con prefijo y responsibas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,10 +4,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/app.js') }}" defer></script>
     <title>Laravel app - @yield('title')</title>
 </head>
 <body>
-    <div>
+    <div class="d-flex flex-column flex-md-row aling-items-center p-3 px-md-4 bg-white border-buttom shadow-sm mb-3">
+        <h5 class="my-0 mr-md-auto font-wight-normal">Laravel app</h5>
+        <nav class="my-2 my-md-0 mr-md-3 mb-3">
+            <a class="p-2 text-dark" href="{{ route('home.index') }}">Home</a>
+            <a class="p-2 text-dark" href="{{ route('home.contact') }}">Contact</a>
+            <a class="p-2 text-dark" href="{{ route('posts.index') }}">Blog post</a>
+            <a class="p-2 text-dark" href="{{ route('posts.create') }}">Add blog post</a>
+        </nav>
+    </div>
+    <div class="container">
+        @if (session('status'))
+            <div style="background-color: red;color: white">
+                {{ session('status') }}
+            </div>
+        @endif
         @yield('content')
     </div>
 </body>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\PostsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,18 +20,6 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // })->name('home.index');
-
-// Route::get('/', function () {
-//     return view('home.index',[]);
-// })->name('home.index');
-
-// Route::get('/contact', function () {
-//     return view('home.contact');;
-// })->name('home.contact');
-
-Route::view('/','home.index')->name('home.index');
-Route::view('/contact','home.contact')->name('home.contact');
-
 $post = [
     1 => [
         'title' => 'intro to laravel',
@@ -47,23 +39,72 @@ $post = [
     ]
 ];
 
-Route::get('/posts', function () use ($post) {
-    
-    //abort_if(!isset($post]),404);
-    return view('post.index',['posts' => $post]);
-})
-->name("post.index");
+Route::get('/', [HomeController::class,'home'])->name('home.index');
 
-Route::get('/post/{id}', function ($id) use ($post) {
-    
-    abort_if(!isset($post[$id]),404);
-    return view('post.show',['post' => $post[$id]]);
-})
-// ->where([
-//     'id' => '[0-9]+'
-// ])
-->name("post.show");
+Route::get('/contact', [HomeController::class,'contact'])->name('home.contact');
 
-Route::get('/resent-post/{id?}', function ($daysago = 20) {
-    return 'post from '.$daysago .' days ago';
-})->name("post.recent.index");
+Route::get("/single", AboutController::class);
+
+// Route::view('/','home.index')->name('home.index');
+// Route::view('/contact','home.contact')->name('home.contact');
+
+Route::resource('posts', PostsController::class);
+//->only(['index','show','create','store','edit','update']);
+//Route::resource('posts', PostsController::class)->except(['index','show']);
+
+// Route::get('/posts', 
+//     //function (Request $request) use ($post) {
+//     function () use ($post) {
+//     //dd(request()->all());
+//     dd((int)request()->input('page',65));
+//     //abort_if(!isset($post]),404);
+//     return view('post.index',['posts' => $post]);
+// })
+// ->name("post.index");
+
+// Route::get('/post/{id}', function ($id) use ($post) {
+    
+//     abort_if(!isset($post[$id]),404);
+//     return view('post.show',['post' => $post[$id]]);
+// })
+// // ->where([
+// //     'id' => '[0-9]+'
+// // ])
+// ->name("post.show");
+
+// Route::get('/resent-post/{id?}', function ($daysago = 20) {
+//     return 'post from '.$daysago .' days ago';
+// })->name("post.recent.index");
+
+
+Route::prefix("/fun")->name("fun.")->group(function () use ($post){
+    Route::get("/fun/responses", function() use ($post) {
+        return response($post,201)
+                ->header('Content-Type','application/json')->cookie('MY_COOKIE','marlon',3600);
+    })->name('response');
+    
+    Route::get("/redirect", function() {
+        return redirect('/contact');
+    })->name('redirect');
+    
+    Route::get("/back", function() {
+        return back();
+    })->name('back');
+    
+    Route::get("/named-route", function() {
+        return redirect()->route('post.show',['id' => 1]);
+    })->name('named-route');
+    
+    Route::get("/away", function() {
+        return redirect()->away('https://www.google.com/');
+    })->name('away');
+    
+    Route::get("/json", function() use ($post) {
+        return response()->json($post);
+    })->name('json');
+    
+    Route::get("/dowload", function() use ($post) {
+        return response()->download(public_path('/gato.png',"dgato.png"));
+    })->name('dowload');
+
+});
