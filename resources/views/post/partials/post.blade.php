39c0a809451389f68d1524d9d95c7fbfe303dcab
marlon olmedo <marlonolmedo240@gmail.com>
2021-04-16 20:23:56 -0600

    cambio estilo
    cambio de estilo de pa pagina principal de los post

--- resources/views/post/partials/post.blade.php
+++ resources/views/post/partials/post.blade.php
@@ -1,5 +1,12 @@
-@if ($loop->even)
-<div>{{$key}}.{{ $post["title"] }}</div>
-@else
-<div style="background-color:silver">{{$key}}.{{ $post["title"] }}</div>
-@endif
+
+<h3>
+    <a href="{{ route('posts.edit',['post' => $post->id]) }}">{{ $post["title"] }}</a>
+</h3>
+<div class="mb-3">
+    <a href="{{ route('posts.edit',['post' => $post->id]) }}" class="btn btn-primary">Edit</a>
+    <form class="d-inline" action="{{ route('posts.destroy',['post' => $post->id]) }}" method="POST">
+        @csrf
+        @method('DELETE')
+        <input type="submit" value="Delete!" class="btn btn-primary">
+    </form>
+</div>
